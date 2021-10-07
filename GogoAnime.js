0d61af85321fe7fcce1951950d2ed7421614670d
Michael24884 <michaelgarcia24884@gmail.com>
2021-10-07 16:35:32 -0700

    Create GogoAnime.js

---
+++ GogoAnime.js
@@ -0,0 +1,16 @@
+
+import { Handheld } from "../src/Models";
+
+
+name = GogoAnime().name;
+getEpisodes = GogoAnime().getEpisodes()
+
+export class GogoAnime extends Handheld {
+    name = "GogoAnime"
+
+    getEpisodes = () => {
+        return "Hello From the gogogANime file"
+    } ;
+
+
+}
